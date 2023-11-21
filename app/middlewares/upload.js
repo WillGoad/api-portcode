a34e06a6caaaed227f232071f9a02b9f02c8a587
Will Goad <will@goad.id>
2023-11-21 18:23:28 +0000

    Fixed GridFS import

--- app/middlewares/upload.js
+++ app/middlewares/upload.js
@@ -1,6 +1,6 @@
 const util = require("util");
 const multer = require('multer');
-const GridFsStorage = require('multer-gridfs-storage');
+const GridFsStorage = require('multer-gridfs-storage').default;
 
 const storage = new GridFsStorage({
   url: process.env.MONGO_URI,
